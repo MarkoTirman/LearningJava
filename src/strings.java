24c215765f7180eb04f46443488e60c78e231e2b
markotirman1 <tirman.marko@gmail.com>
2026-06-21 17:18:23 +0300

    Strings and manipulation

---
+++ src/strings.java
@@ -0,0 +1,9 @@
+public class strings {
+    public static void main(String[] args){
+        String message = "  Hello world" + "!!  ";
+        System.out.println(message.length());
+        System.out.println(message.toUpperCase());
+        System.out.println(message.toLowerCase());
+        System.out.println(message.trim());
+    }
+}
