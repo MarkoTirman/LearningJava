dcfccb044ddd28a03c48eabd2ac656c54e6c44c5
markotirman1 <tirman.marko@gmail.com>
2026-06-21 17:26:49 +0300

    escaping sequences, which ones are useful and mainly used

---
+++ src/escapeSequences.java
@@ -0,0 +1,8 @@
+public class escapeSequences {
+
+    public static void main(String[] args){
+        // c:\windows\...
+        String message = "c:\\Windows\\..."; // to escape remember these 4: \\, \n, \t, \"Mosh\""
+        System.out.println(message);
+    }
+}
